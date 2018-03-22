1f26e3dfe73807421f72539507594a2444417230
Prazma Repos <tikitikiteke.newgrounds@gmail.com>
2018-03-22 17:21:31 +0900

    Update server.js

--- js/server.js
+++ js/server.js
@@ -1,12 +1,37 @@
 var datavase = new Firebase('https://peer2peer-60587.firebaseio.com');
-firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    getUserData();
-  } else {
-    shouLogin();
-  }
-});
+var readerMode = 0;
 
-function shouLogin() {
-  
+function signup() {
+  var username = document.getElementById("newusername").value;
+  var email = document.getElementById("newemail").value;
+  var password = document.getElementById("newpassword").value;
+
+  datavase.push({datatype:"account", usercode:username, username:username, email:email, password:password, gold:0, levelVocab: 0, levelGram: 0});
+
+  localStorage.loggedin = "true";
+  localStorage.uid = username;
+
+  document.getElementById("guestUserContent").style.display = "none";
+  document.getElementById("loggedUserContent").style.display = "block";
+  signupModal.style.display = "none";
+  loginModal.style.display = "none";
+
+}
+
+var startListening = function() {
+	datavase.on('child_added', function(snapshot) {
+
+  });
+}
+startListening();
+
+window.onload = function () {
+  if( localStorage.loggedin == "true" ) {
+    document.getElementById("guestUserContent").style.display = "none";
+    document.getElementById("loggedUserContent").style.display = "block";
+    signupModal.style.display = "none";
+    loginModal.style.display = "none";
+    document.getElementById("usernameBig").innerHTML = localStorage.uid;
+    document.getElementById("usernameMini").innerHTML = localStorage.uid;
+  }
 }
