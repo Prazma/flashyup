49f86561a408b5e45f2ae08b13a6739f11801581
thecodepal <37014685+thecodepal@users.noreply.github.com>
2018-03-23 22:06:33 +0900

    Update server.js

--- js/server.js
+++ js/server.js
@@ -1,5 +1,9 @@
 var datavase = new Firebase('https://peer2peer-60587.firebaseio.com');
 var readerMode = 0;
+var accountEmails = [];
+var accountUsername = [];
+var accountPasswords = [];
+var cred = document.getElementById("loginInitBtn");
 
 function signup() {
   var username = document.getElementById("newusername").value;
@@ -16,11 +20,20 @@ function signup() {
   signupModal.style.display = "none";
   loginModal.style.display = "none";
 
+
+  document.getElementById("usernameBig").innerHTML = localStorage.uid;
+  document.getElementById("usernameMini").innerHTML = localStorage.uid;
+
 }
 
 var startListening = function() {
 	datavase.on('child_added', function(snapshot) {
-
+    var fdata = snapshot.val();
+    if( fdata.datatype  == "account" ) {
+      accountEmails.push( fdata.email );
+      accountPasswords.push( fdata.password );
+      accountUsername.push( fdata.username );
+    }
   });
 }
 startListening();
@@ -33,5 +46,36 @@ window.onload = function () {
     loginModal.style.display = "none";
     document.getElementById("usernameBig").innerHTML = localStorage.uid;
     document.getElementById("usernameMini").innerHTML = localStorage.uid;
+    var windowWidth = window.innerWidth;
+    document.html.style.width = windowWidth + "px";
+  }
+}
+cred.onclick = function () {
+  var credE = document.getElementById("loginEmailCred").value;
+  var credP = document.getElementById("loginPassCred").value;
+  var tcpik = accountEmails.indexOf( credE );
+  if( tcpik != -1 ) {
+    var fcpik = accountPasswords[tcpik];
+    var dfcpik = accountPasswords.indexOf( fcpik );
+    var til = accountPasswords.indexOf( credP );
+    if( dfcpik == til ) {
+      var username = accountUsername[til];
+      document.getElementById("annoyingLoginErrorMessage").style.display = "none";
+      localStorage.loggedin = "true";
+      localStorage.uid = username;
+
+      document.getElementById("guestUserContent").style.display = "none";
+      document.getElementById("loggedUserContent").style.display = "block";
+      signupModal.style.display = "none";
+      loginModal.style.display = "none";
+
+
+      document.getElementById("usernameBig").innerHTML = localStorage.uid;
+      document.getElementById("usernameMini").innerHTML = localStorage.uid;
+    } else {
+      document.getElementById("annoyingLoginErrorMessage").style.display = "inline";
+    }
+  } else {
+    document.getElementById("annoyingLoginErrorMessage").style.display = "inline";
   }
 }
